927502c55538f20db8c8be9cc1b11a75394d8185
LucasStf <lucas-strafe@protonmail.com>
2023-04-30 15:23:22 +0200

    Project base

---
+++ GSCoder/GSCoder.Gtk/Program.cs
@@ -0,0 +1,14 @@
+﻿using Eto.Forms;
+using System;
+
+namespace GSCoder.Gtk
+{
+    internal class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            new Application(Eto.Platforms.Gtk).Run(new MainForm());
+        }
+    }
+}
---
+++ GSCoder/GSCoder.Wpf/Program.cs
@@ -0,0 +1,14 @@
+﻿using Eto.Forms;
+using System;
+
+namespace GSCoder.Wpf
+{
+    internal class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            new Application(Eto.Platforms.Wpf).Run(new MainForm());
+        }
+    }
+}
